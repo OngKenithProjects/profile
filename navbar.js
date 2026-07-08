e4b8fbb9829f1b4a3488d25e0809f7f884c9ac5c
ongkenith <ongkenith2@gmail.com>
2026-07-08 11:05:07 +0800

    Collapsable nav bar

--- navbar.js
+++ navbar.js
@@ -2,6 +2,9 @@ function renderNavbar(active) {
   return `
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
       <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item" style="margin-right:5px">
